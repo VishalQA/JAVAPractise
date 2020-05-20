5bae81b225af709b3351065a697fce9edda7a8f2
VishalQA <vishal.mailbox1@gmail.com>
2020-05-20 21:47:16 +0530

    ok

---
+++ JAVAProjectone/src/packageone/ThisDemo.java
@@ -0,0 +1,18 @@
+package packageone;
+
+public class ThisDemo {
+	
+	int id;
+	String name ;
+	
+	ThisDemo(int id, String name){
+		this.id = id;
+	  this.name = name;
+								
+	}
+	void display() {
+		System.out.println(id+ "  "  +name);
+	}
+	
+
+}
---
+++ JAVAProjectone/src/packageone/ThisDemoTest.java
@@ -0,0 +1,12 @@
+package packageone;
+
+public class ThisDemoTest {
+
+	public static void main(String[] args) {
+		
+		ThisDemo t  = new ThisDemo(100 , "John");
+		t.display();
+
+	}
+
+}
